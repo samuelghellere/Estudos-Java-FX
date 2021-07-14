977706600afadac50606bcf7c3d0823c6023cfbb
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-07-14 07:26:13 -0300

    Implementando um button e utilizando um evento onAction

---
+++ javaFX2/src/gui/ViewController.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+
+	@FXML
+	private Button btTeste;
+	
+	@FXML
+	public void onBtTesteAction() {
+		System.out.println("Click");
+	}
+}
